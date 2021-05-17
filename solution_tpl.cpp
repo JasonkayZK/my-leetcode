58ca3a15f75ab27dfa30cbd04882ccfc62c67242
JasonkayZK <271226192@qq.com>
2021-05-17 17:55:53 +0800

    finished: 993

--- solution_tpl.cpp
+++ solution_tpl.cpp
@@ -6,7 +6,24 @@
 
 using namespace std;
 
+// Definition for a binary tree node.
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
 class Solution {
+private:
+
 public:
 
 };
