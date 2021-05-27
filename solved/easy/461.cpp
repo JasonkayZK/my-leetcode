0740b32f20d338340c9a7bbecbc3d6a3a96d7013
JasonkayZK <271226192@qq.com>
2021-05-27 17:10:03 +0800

    update 461

--- solved/easy/461.cpp
+++ solved/easy/461.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 class Solution {
 public:
+/* Solution 1: default
     int hammingDistance(int x, int y) {
         int c = x^y;
         int res = 0;
@@ -17,4 +18,29 @@ public:
         }
         return res;
     }
+*/
+
+/* Solution 2: Brian Kernighan
+    int hammingDistance(int x, int y) {
+        int s = x ^ y, ret = 0;
+        while (s) {
+            s &= s - 1;
+            ret++;
+        }
+        return ret;
+    }
+*/
+
+/* Solution 3: 分治 */
+    int hammingDistance(int x, int y) {
+        int c;
+        c = x ^ y;
+        c = (c & 0x55555555) + ((c >> 1) & 0x55555555);
+        c = (c & 0x33333333) + ((c >> 2) & 0x33333333);
+        c = (c & 0x0f0f0f0f) + ((c >> 4) & 0x0f0f0f0f);
+        c = (c & 0x00ff00ff) + ((c >> 8) & 0x00ff00ff);
+        c = (c & 0x0000ffff) + ((c >> 16) & 0x0000ffff);
+        return c;
+    }
+
 };
