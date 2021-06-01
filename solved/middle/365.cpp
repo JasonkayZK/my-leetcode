afd779ffb70f7cf8ef13a790205c9c7248c2200f
JasonkayZK <271226192@qq.com>
2021-06-01 15:41:47 +0800

    finished: 365

---
+++ solved/middle/365.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <numeric>
+#include <stack>
+#include <unordered_set>
+
+using namespace std;
+
+using PII = pair<int, int>;
+
+// Definition for a binary tree node.
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+
+public:
+/* Method 1: DFS
+    bool canMeasureWater(int x, int y, int z) {
+        stack<PII> stk;
+        stk.emplace(0, 0);
+        auto hash_function = [](const PII &o) { return hash<int>()(o.first) ^ hash<int>()(o.second); };
+        unordered_set<PII, decltype(hash_function)> seen(0, hash_function);
+
+        while (!stk.empty()) {
+            if (seen.count(stk.top())) {
+                stk.pop();
+                continue;
+            }
+            seen.emplace(stk.top());
+
+            auto [remain_x, remain_y] = stk.top();
+
+            stk.pop();
+            if (remain_x == z || remain_y == z || remain_x + remain_y == z) {
+                return true;
+            }
+            // 把 X 壶灌满。
+            stk.emplace(x, remain_y);
+            // 把 Y 壶灌满。
+            stk.emplace(remain_x, y);
+            // 把 X 壶倒空。
+            stk.emplace(0, remain_y);
+            // 把 Y 壶倒空。
+            stk.emplace(remain_x, 0);
+            // 把 X 壶的水灌进 Y 壶，直至灌满或倒空。
+            stk.emplace(remain_x - min(remain_x, y - remain_y), remain_y + min(remain_x, y - remain_y));
+            // 把 Y 壶的水灌进 X 壶，直至灌满或倒空。
+            stk.emplace(remain_x + min(remain_y, x - remain_x), remain_y - min(remain_y, x - remain_x));
+        }
+        return false;
+    }
+ */
+/* Method 2: Math 贝祖定理:
+    https://baike.baidu.com/item/%E8%A3%B4%E8%9C%80%E5%AE%9A%E7%90%86/5186593?fromtitle=%E8%B4%9D%E7%A5%96%E5%AE%9A%E7%90%86&fromid=5185441
+*/
+public:
+    bool canMeasureWater(int x, int y, int z) {
+        if (x + y < z) return false;
+        if (z == 0 || (x + y) == z) return true;
+
+        auto d = std::gcd(x, y);
+        return z % d == 0;
+    }
+};
+
+int main() {
+
+
+    return 0;
+}
