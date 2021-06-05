acda114a9f87116e47e71b9f91310ff18aa58cae
JasonkayZK <271226192@qq.com>
2021-06-05 13:55:52 +0800

    finished: 70

---
+++ solved/easy/70.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <numeric>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+/* DP */
+public:
+
+    int climbStairs(int n) {
+        if (n == 0 || n == 1) return 1;
+
+        int first, second = 1, third = 1;
+        for (int i = 2; i <= n; i++) {
+            first = second;
+            second = third;
+            third = (first + second);
+        }
+        return third;
+    }
+};
---
+++ solved/easy/offer-10-II.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <numeric>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+/* DP */
+public:
+    const int mod = 1000000007;
+
+    int numWays(int n) {
+        if (n == 0 || n == 1) return 1;
+
+        int first, second = 1, third = 1;
+        for (int i = 2; i <= n; i++) {
+            first = second;
+            second = third;
+            third = (first + second) % 1000000007;
+        }
+        return third;
+    }
+};
