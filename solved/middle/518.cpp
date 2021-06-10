4afdfcf7b2daa98576df925e28e995c7285c48ce
JasonkayZK <271226192@qq.com>
2021-06-10 12:34:20 +0800

    solved: 518

---
+++ solved/middle/518.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <numeric>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+/* DP */
+public:
+    int change(int amount, vector<int> &coins) {
+        vector<int> dp(amount + 1);
+        dp[0] = 1;
+        for (int &coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+};
