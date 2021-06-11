eabb80eb525c51fb7c2319486c44ccb954119ac3
JasonkayZK <271226192@qq.com>
2021-06-11 15:28:43 +0800

    update template

--- solution_tpl.cpp
+++ solution_tpl.cpp
@@ -3,8 +3,16 @@
 #include <map>
 #include <vector>
 #include <memory>
+#include <cmath>
+#include <numeric>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
+
+using PII = pair<int, int>;
+using LL = long long;
 
 // Definition for a binary tree node.
 class TreeNode {
@@ -20,6 +28,18 @@ public:
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+// Definition for singly-linked list.
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 class Solution {
 private:
@@ -29,7 +49,6 @@ public:
 };
 
 int main() {
-
 
     return 0;
 }
