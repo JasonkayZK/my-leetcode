8084ce376f6aea3d40922105a731b20187124cfc
JasonkayZK <271226192@qq.com>
2021-08-09 23:04:01 +0800

    finished: 313

---
+++ solved/middle/313.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <numeric>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <climits>
+#include <queue>
+
+using namespace std;
+
+using LL = long long;
+
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        priority_queue<LL, vector<LL>, greater<LL>> pq;
+        LL res = 1;
+        for (int i = 1; i < n; ++i) {
+            for (auto& prime : primes) {
+                pq.push(prime * res);
+            }
+            res = pq.top();
+            pq.pop();
+            while(pq.size() && res == pq.top()) pq.pop();
+        }
+        return (int)res;
+    }
+};
