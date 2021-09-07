f70f006c78c21d6477e7fb2c0a0a3f80c5bcbf19
JasonkayZK <271226192@qq.com>
2021-09-07 15:58:08 +0800

    finished: 1221

---
+++ solved/easy/1221.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <numeric>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <climits>
+#include <queue>
+#include <random>
+
+using namespace std;
+
+using PII = pair<int, int>;
+using LL = long long;
+using PIC = pair<int, char>;
+
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int res = 0, curCnt = 0;
+        for (char i : s) {
+            if ('R' == i) curCnt++;
+            else curCnt--;
+            if (curCnt == 0) res++;
+        }
+        return res;
+    }
+};
