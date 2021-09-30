ddeabe2c4bde04fb55e4d4680bdf5c9fdf464a19
JasonkayZK <271226192@qq.com>
2021-09-30 09:16:33 +0800

    finished: 223

---
+++ solved/middle/223.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <numeric>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <climits>
+#include <queue>
+#include <random>
+
+using namespace std;
+
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1 = (ax2 - ax1) * (ay2 - ay1), area2 = (bx2 - bx1) * (by2 - by1);
+        int overlapWidth = min(ax2, bx2) - max(ax1, bx1), overlapHeight = min(ay2, by2) - max(ay1, by1);
+        int overlapArea = max(overlapWidth, 0) * max(overlapHeight, 0);
+        return area1 + area2 - overlapArea;
+    }
+};
