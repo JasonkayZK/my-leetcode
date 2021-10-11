a410178f6f872bee71b48571ba78970ac0bfcf33
JasonkayZK <271226192@qq.com>
2021-10-11 23:34:37 +0800

    solved: 273

---
+++ solved/hard/273.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <numeric>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <climits>
+#include <queue>
+
+using namespace std;
+
+using PII = pair<int, int>;
+using LL = long long;
+using PIC = pair<int, char>;
+
+// Definition for a binary tree node.
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// Definition for singly-linked list.
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void print_vec(vector<int> arr) {
+    std::for_each(arr.begin(), arr.end(), [](const auto &i) { std::cout << i << " "; });
+    cout << "\n";
+}
+
+void swap(vector<int> &nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+class Solution {
+public:
+    vector<string> singles = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    vector<string> teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    vector<string> tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    vector<string> thousands = {"", "Thousand", "Million", "Billion"};
+
+    string numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+        string sb;
+        for (int i = 3, unit = 1000000000; i >= 0; i--, unit /= 1000) {
+            int curNum = num / unit;
+            if (curNum != 0) {
+                num -= curNum * unit;
+                sb = sb + toEnglish(curNum) + thousands[i] + " ";
+            }
+        }
+        while (sb.back() == ' ') {
+            sb.pop_back();
+        }
+        return sb;
+    }
+
+    string toEnglish(int num) {
+        string curr;
+        int hundred = num / 100;
+        num %= 100;
+        if (hundred != 0) {
+            curr = curr + singles[hundred] + " Hundred ";
+        }
+        int ten = num / 10;
+        if (ten >= 2) {
+            curr = curr + tens[ten] + " ";
+            num %= 10;
+        }
+        if (num > 0 && num < 10) {
+            curr = curr + singles[num] + " ";
+        } else if (num >= 10) {
+            curr = curr + teens[num - 10] + " ";
+        }
+        return curr;
+    }
+};
