887a6001bcba581b78bee6ab1d7889ab8752566e
JasonkayZK <271226192@qq.com>
2022-04-14 00:08:03 +0800

    temp save

---
+++ solved/easy/190.cpp
@@ -0,0 +1,27 @@
+#include "utils/utils.h"
+#include "data_structure/list.h"
+#include "data_structure/tree.h"
+#include "data_structure/trie.h"
+#include "data_structure/segment_tree.h"
+
+class Solution {
+private:
+
+public:
+
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        for (int i = 0; i < 32 && n > 0; ++i) {
+            res |= (n & 1) << (31 - i);
+            n >>= 1;
+        }
+
+        return res;
+    }
+
+};
+
+int main() {
+
+    return 0;
+}
