f41a009260d6bf5ceee112e85f3b89fd00c2f640
JasonkayZK <271226192@qq.com>
2022-05-13 21:57:44 +0800

    temp

---
+++ solved/easy/551.cc
@@ -0,0 +1,29 @@
+#include "utils/utils.h"
+#include "data_structure/list.h"
+#include "data_structure/tree.h"
+#include "data_structure/trie.h"
+#include "data_structure/segment_tree.h"
+#include "data_structure/union_find.h"
+
+class Solution {
+public:
+
+    bool checkRecord(string s) {
+        int a_cnt = 0;
+        for (int i = 0, n = int(s.size()); i < n; ++i) {
+            if (s[i] == 'A') ++a_cnt;
+            else if (i < n - 2 && s[i] == 'L' && s[i + 1] == 'L' && s[i + 2] == 'L') return false;
+        }
+        return a_cnt < 2;
+    }
+
+private:
+
+
+};
+
+int main() {
+
+
+    return 0;
+}
